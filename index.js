970b1b9d1b4835ea1b8a17604f14c472315b3a39
tandonk <kartik.tandon@tabcorp.com.au>
2025-01-18 13:07:13 +0530

    post method for chat gpt

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 import { GoogleGenerativeAI  } from '@google/generative-ai';
 import dotenv from 'dotenv';
+import express from 'express'
 dotenv.config();
 const genai = new GoogleGenerativeAI(process.env.APi_KEY)
+const app = express()
+const model = genai.getGenerativeModel({model:"gemini-1.5-flash"});
+import bodyParser from 'body-parser';
 
-const model = genai.getGenerativeModel({model:"gemini-1.5-flash"})
+app.use(bodyParser.json())
 
-import readlineSync from 'readline-sync';
-import colors from 'colors';
-
-
-async function generate(){
-  console.log(colors.bold.green('Welcome to the Chatbot Program!'));
-  console.log(colors.bold.green('You can start chatting with the bot.'));
-  while (true) {
-    try{
-        const userInput = readlineSync.question(colors.yellow('You: '));
-        const result  = await model.generateContent(userInput);
-        const completionText = result.response.text()
-        if (userInput.toLowerCase() === 'exit') {
-          console.log(colors.green('Bot: ') + completionText);
-          return;
-        }
-        console.log(colors.green('Bot: ') + completionText);
-      }
-    catch(err){
-      console.log(err)
-    }
-  }
+ function Generate(){
+  app.post('/', async(req,res) => {
+   const response = await getData(req.body.question)
+    res.send(response)
+  })
 
 }
+async function getData(question){
+  const result  = await model.generateContent(question);
+        const completionText = result.response.text()
+        return completionText
+}
+Generate()
 
-generate()
+app.listen('3000', ()=>{
+  console.log('server restarted')
+})
