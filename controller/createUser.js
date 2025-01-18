1f9c0d65567ac4e5d878cd5680bb4838a5fd58b3
tandonk <kartik.tandon@tabcorp.com.au>
2025-01-19 00:14:36 +0530

    signup page

---
+++ controller/createUser.js
@@ -0,0 +1,4 @@
+export default async function createUser(req,res){
+  const {username, email,password} = req.body
+
+}
--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const app = express()
 import bodyParser from 'body-parser';
 import {router as dataRoute} from './router/data.js'
 import { router as homeRoute } from './router/home.js';
+import {router as signRoute} from './router/signup.js'
 import path from 'path'
-
+app.use(bodyParser.urlencoded({ extended:false}))
 app.use(bodyParser.json())
 app.set("view engine", "ejs")
 app.set("views", path.resolve("./views") )
 app.use('/data', dataRoute)
 app.use('/', homeRoute)
-
+app.use('/signup',signRoute)
 app.listen('3000', ()=>{
   console.log('server restarted')
 })
